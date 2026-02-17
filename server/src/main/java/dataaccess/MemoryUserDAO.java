ab8996ea807c7385f9a6a1586fadfd536104d8c1
blakeg9944 <blakeg9944@gmail.com>
2026-02-17 16:44:40 -0700

    MemoryUserDAO is implemented

--- server/src/main/java/dataaccess/MemoryUserDAO.java
+++ server/src/main/java/dataaccess/MemoryUserDAO.java
@@ -4,7 +4,22 @@ import model.UserData;
 
 import java.util.HashMap;
 
-//public class MemoryUserDAO implements UserDAO {
-    //private final HashMap<String, >
+public class MemoryUserDAO implements UserDAO {
+    private final HashMap<String, UserData > users = new HashMap<>();
 
-//}
+    @Override
+    public void createUser(UserData u){
+        users.put(u.username(),u);
+    }
+
+    @Override
+    public UserData getUser(String username) throws DataAccessException {
+        return users.get(username);
+    }
+
+    @Override
+    public void clear() throws DataAccessException {
+        users.clear();
+    }
+
+}
