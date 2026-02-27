9ff48c5c3cd961a4aa1763e22c4594e2eff0051f
blakeg9944 <blakeg9944@gmail.com>
2026-02-27 09:20:33 -0700

    Phase 3: Creating files for create game

---
+++ server/src/main/java/service/CreateGameService.java
@@ -0,0 +1,12 @@
+package service;
+
+import model.CreateGameRequest;
+import model.CreateGameResult;
+import model.ListGamesResult;
+
+public class CreateGameService {
+
+    public CreateGameResult createGame(CreateGameRequest createGameRequest){
+
+    }
+}
