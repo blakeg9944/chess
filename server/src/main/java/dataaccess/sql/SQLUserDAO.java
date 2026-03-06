2a05ee516e32e4681f7ba5e4fc0fdec1296ef21d
blakeg9944 <blakeg9944@gmail.com>
2026-03-06 11:19:43 -0700

    Phase 4: getUser and createUser

--- server/src/main/java/dataaccess/sql/SQLUserDAO.java
+++ server/src/main/java/dataaccess/sql/SQLUserDAO.java
@@ -1,4 +1,51 @@
 package dataaccess.sql;
 
+import dataaccess.DataAccessException;
+import dataaccess.DatabaseManager;
+import model.UserData;
+
+import java.sql.PreparedStatement;
+
+import java.sql.*;
+
 public class SQLUserDAO {
+
+    public void createUser(UserData user) throws DataAccessException{
+        String statement = "INSERT INTO users (username, password, email) VALUES (?, ?, ?)";
+        try(Connection connection = DatabaseManager.getConnection()){
+            PreparedStatement preparedStatement = connection.prepareStatement(statement);
+            preparedStatement.setString(1, user.username());
+            preparedStatement.setString(2, user.password());
+            preparedStatement.setString(3, user.email());
+            preparedStatement.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public UserData getUser(String username){
+        String statement = "SELECT username, password, email FROM users WHERE username = ?";
+        try(Connection connection = DatabaseManager.getConnection()){
+            try (PreparedStatement ps = connection.prepareStatement(statement)) {
+                ps.setString(1, username);
+                try (ResultSet rs = ps.executeQuery()) {
+                    if (rs.next()) {
+                        return readUser(rs);
+                    }
+                    return null;
+                }
+            }
+        } catch (SQLException | DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private UserData readUser(ResultSet rs) throws SQLException {
+        String username = rs.getString("username");
+        String password = rs.getString("password");
+        String email = rs.getString("email");
+        return new UserData(username, password, email);
+    }
+
 }
