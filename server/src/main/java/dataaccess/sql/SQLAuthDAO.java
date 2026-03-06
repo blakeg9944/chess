a5667510201e2a30ac3cfa1e6141454a8e557586
blakeg9944 <blakeg9944@gmail.com>
2026-03-06 15:52:27 -0700

    Phase 4: halfway done with the auth DAO

--- server/src/main/java/dataaccess/sql/SQLAuthDAO.java
+++ server/src/main/java/dataaccess/sql/SQLAuthDAO.java
@@ -1,35 +1,72 @@
 package dataaccess.sql;
 
 import dataaccess.DataAccessException;
+import dataaccess.DatabaseManager;
+import dataaccess.interfaces.AuthDAO;
 import dataaccess.interfaces.GameDAO;
+import model.AuthData;
 import model.GameData;
+import model.UserData;
+import service.UnauthorizedException;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Collection;
 import java.util.List;
 
-public class SQLAuthDAO implements GameDAO {
+public class SQLAuthDAO implements AuthDAO {
+
     @Override
-    public int createGame(String gameName) throws DataAccessException {
-        return 0;
+    public void createAuth(AuthData a) throws DataAccessException {
+        String statement = "INSERT INTO auth (authtoken, username) VALUES (?, ?)";
+        try(Connection connection = DatabaseManager.getConnection()) {
+            PreparedStatement preparedStatement = connection.prepareStatement(statement);
+            preparedStatement.setString(1, a.authToken());
+            preparedStatement.setString(2, a.username());
+        }
+        catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
-    public GameData getGame(Integer gameID) throws DataAccessException {
-        return null;
+    public AuthData getAuth(String authToken) throws DataAccessException {
+        String statement = "SELECT username, authtoken FROM auth WHERE authtoken = ?";
+        try(Connection connection = DatabaseManager.getConnection()) {
+            try (PreparedStatement ps = connection.prepareStatement(statement)) {
+                ps.setString(1, authToken);
+                try (ResultSet rs = ps.executeQuery()) {
+                    if (rs.next()) {
+                        return readAuth(rs);
+                    }
+                    return null;
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
-    public Collection<GameData> listGames() throws DataAccessException {
-        return List.of();
-    }
-
-    @Override
-    public void updateGame(GameData g) throws DataAccessException {
+    public void deleteAuth(String authToken) throws DataAccessException {
 
     }
 
     @Override
     public void clear() throws DataAccessException {
 
+    }
+
+    @Override
+    public void verifyToken(String authToken, AuthData a) throws UnauthorizedException {
+
+    }
+
+    private AuthData readAuth(ResultSet rs) throws SQLException {
+        String username = rs.getString("username");
+        String authtoken = rs.getString("authtoken");
+        return new AuthData(authtoken, username);
     }
 }
