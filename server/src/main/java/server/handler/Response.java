5ac796fda6a2b7ef6d9f9c85fd8ee594e5b5ac23
blakeg9944 <blakeg9944@gmail.com>
2026-03-11 00:46:30 -0600

    Phase 4: trying to catch requests better

--- server/src/main/java/server/handler/Response.java
+++ server/src/main/java/server/handler/Response.java
@@ -2,6 +2,7 @@ package server.handler;
 
 import com.google.gson.Gson;
 import io.javalin.http.Context;
+import service.AlreadyTakenException;
 import service.BadRequestException;
 import service.UnauthorizedException;
 
@@ -13,20 +14,24 @@ public class Response {
     public static void success(Context ctx, Object response){
         String jsonResponse = gson.toJson(response);
         ctx.result(jsonResponse);
-    };
+    }
 
-    public static void failure(Context ctx, Exception e){
-        if (e instanceof BadRequestException){
+    public static void failure(Context ctx, Exception e) {
+        if (e instanceof BadRequestException) {
             ctx.status(400);
-            ctx.result(gson.toJson(Map.of("message", "Error: bad request")));
+            ctx.result(gson.toJson(Map.of("message", e.getMessage())));
         }
         else if (e instanceof UnauthorizedException) {
             ctx.status(401);
-            ctx.result(gson.toJson(Map.of("message", "Error: unauthorized")));
+            ctx.result(gson.toJson(Map.of("message", e.getMessage())));
         }
-        else{
+        else if (e instanceof AlreadyTakenException) {
+            ctx.status(403);
+            ctx.result(gson.toJson(Map.of("message", e.getMessage())));
+        }
+        else {
             ctx.status(500);
-            ctx.result(gson.toJson(Map.of("message", "Error: (description of error)")));
+            ctx.result(gson.toJson(Map.of("message", "Error: " + e.getMessage())));
         }
     }
 }
