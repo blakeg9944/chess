954918f5ebb78c8e9088c4cb8fe8ceb1bfb986da
blakeg9944 <blakeg9944@gmail.com>
2026-03-14 16:28:39 -0600

    Phase 5: Initial attempt at eval

---
+++ client/src/main/java/client/ChessClient.java
@@ -0,0 +1,40 @@
+package client;
+
+import java.util.Arrays;
+
+
+public class ChessClient {
+
+    private final ServerMain server;
+    private final String serverUrl;
+
+    public ChessClient(String serverUrl) {
+        this.serverUrl = serverUrl;
+        // Initialize the facade that handles HTTP requests
+        this.server = new ServerFacade(serverUrl);
+    }
+    public String eval(String input) {
+        try {
+            String[] tokens = input.toLowerCase().split(" ");
+            String cmd = (tokens.length > 0) ? tokens[0] : "help";
+            String[] params = Arrays.copyOfRange(tokens, 1, tokens.length);
+            return switch (cmd) {
+                case "register" -> register(params);
+                case "login" -> login(params);
+                case "quit" -> "quit";
+                default -> help();
+            };
+        } catch (Exception ex) {
+            return ex.getMessage();
+        }
+    }
+
+    public String help() {
+        return """
+            register <USERNAME> <PASSWORD> <EMAIL> - to create an account
+            login <USERNAME> <PASSWORD> - to play chess
+            quit - playing chess
+            help - with possible commands
+            """;
+    }
+}
---
+++ client/src/main/java/client/ReadEvalPrint.java
@@ -0,0 +1,34 @@
+package client;
+
+import java.util.Scanner;
+
+public class ReadEvalPrint {
+
+    private final ChessClient client;
+
+    public ReadEvalPrint(ChessClient client) {
+        client = new ChessClient(serverUrl);
+    }
+
+
+    public void run(){
+        System.out.println("Welcome to 240 Chess. Type Help to get started.");
+        Scanner scanner = new Scanner(System.in);
+        var result = "";
+        while (!result.equals("quit")){
+            printPrompt();
+            String line = scanner.nextLine();
+            try{
+                result = client.eval(line);
+                System.out.print(result);
+            }
+            catch(Throwable e){
+                System.out.print(e.getMessage());
+            }
+        }
+    }
+
+    public void printPrompt(){
+        System.out.print("/n" + "[LOGGED_OUT] >>> ");
+    }
+}
