72343d9fcfe4c4f76161c30b1b90f04b7f52cfae
blakeg9944 <blakeg9944@gmail.com>
2026-03-16 12:52:59 -0600

    Phase 5:  serverfacade- register

--- client/src/main/java/client/ServerFacade.java
+++ client/src/main/java/client/ServerFacade.java
@@ -3,6 +3,8 @@ package client;
 import com.google.gson.Gson;
 import model.LoginRequest;
 import model.LoginResult;
+import model.RegisterRequest;
+import model.RegisterResult;
 
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -38,6 +40,29 @@ public class ServerFacade {
                 InputStreamReader inputStreamReader = new InputStreamReader(responseBody);
                 // 5. Return the Result object
                 return new Gson().fromJson(inputStreamReader, LoginResult.class);
+            }
+        }
+        else{
+            throw new Exception("Error: " + http.getResponseCode());
+        }
+    }
+
+    public RegisterResult register(RegisterRequest registerRequest) throws Exception{
+        URL url = new URI(serverUrl + "/user").toURL();
+        HttpURLConnection http = (HttpURLConnection) url.openConnection();
+        // 2. Set Method to "POST"
+        http.setRequestMethod("POST");
+        http.setDoOutput(true);
+        try(var outputStream = http.getOutputStream()){
+            var jsonBody = new Gson().toJson(registerRequest);
+            outputStream.write(jsonBody.getBytes());
+        }
+        http.connect();
+        if (http.getResponseCode() == 200){
+            try (InputStream responseBody = http.getInputStream()){
+                InputStreamReader inputStreamReader = new InputStreamReader(responseBody);
+                // 5. Return the Result object
+                return new Gson().fromJson(inputStreamReader, RegisterResult.class);
             }
         }
         else{
