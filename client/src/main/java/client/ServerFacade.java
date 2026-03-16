401fb586e98e95e5a4a232f998b40206e3c5c9a8
blakeg9944 <blakeg9944@gmail.com>
2026-03-16 11:55:23 -0600

    Phase 5: Started serverfacade

---
+++ client/src/main/java/client/ServerFacade.java
@@ -0,0 +1,47 @@
+package client;
+
+import com.google.gson.Gson;
+import model.LoginRequest;
+import model.LoginResult;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URL;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class ServerFacade {
+    private final String serverUrl;
+
+    public ServerFacade(String url) {
+        this.serverUrl = url;
+    }
+
+    public LoginResult login(LoginRequest request) throws Exception {
+        // 1. Create URL and Connection
+        URL url = new URI(serverUrl + "/session").toURL();
+        HttpURLConnection http = (HttpURLConnection) url.openConnection();
+        // 2. Set Method to "POST"
+        http.setRequestMethod("POST");
+        http.setDoOutput(true);
+        // 3. Write JSON request body
+        try(var outputStream = http.getOutputStream()){
+            var jsonBody = new Gson().toJson(request);
+            outputStream.write(jsonBody.getBytes());
+        }
+        http.connect();
+        // 4. Read JSON response body
+        if (http.getResponseCode() == 200){
+            try (InputStream responseBody = http.getInputStream()){
+                InputStreamReader inputStreamReader = new InputStreamReader(responseBody);
+                // 5. Return the Result object
+                return new Gson().fromJson(inputStreamReader, LoginResult.class);
+            }
+        }
+        else{
+            throw new Exception("Error: " + http.getResponseCode());
+        }
+    }
+}
