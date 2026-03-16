e072e576aa673abc4f7f40611ab84443d18a36a4
blakeg9944 <blakeg9944@gmail.com>
2026-03-16 15:40:25 -0600

    Phase 5:  serverfacade- listgames and logout

--- client/src/main/java/client/ServerFacade.java
+++ client/src/main/java/client/ServerFacade.java
@@ -1,18 +1,13 @@
 package client;
 
 import com.google.gson.Gson;
-import model.LoginRequest;
-import model.LoginResult;
-import model.RegisterRequest;
-import model.RegisterResult;
+import model.*;
 
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URI;
 import java.net.URL;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
 
 public class ServerFacade {
     private final String serverUrl;
@@ -63,6 +58,43 @@ public class ServerFacade {
                 InputStreamReader inputStreamReader = new InputStreamReader(responseBody);
                 // 5. Return the Result object
                 return new Gson().fromJson(inputStreamReader, RegisterResult.class);
+            }
+        }
+        else{
+            throw new Exception("Error: " + http.getResponseCode());
+        }
+    }
+
+    public void logout(String authToken) throws Exception{
+        URL url = new URI(serverUrl + "/session").toURL();
+        HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+        httpURLConnection.setRequestMethod("DELETE");
+        httpURLConnection.setRequestProperty("authorization", authToken);
+        httpURLConnection.connect();
+        if(httpURLConnection.getResponseCode() != 200) {
+            throw new Exception("Logout failed: " + httpURLConnection.getResponseCode());
+        }
+    }
+
+    public ListGamesResult listGames(ListGamesRequest listGamesRequest) throws Exception{
+        // 1. Create URL and Connection
+        URL url = new URI(serverUrl + "/game").toURL();
+        HttpURLConnection http = (HttpURLConnection) url.openConnection();
+        // 2. Set Method to "POST"
+        http.setRequestMethod("GET");
+        http.setDoOutput(true);
+        // 3. Write JSON request body
+        try(var outputStream = http.getOutputStream()){
+            var jsonBody = new Gson().toJson(listGamesRequest);
+            outputStream.write(jsonBody.getBytes());
+        }
+        http.connect();
+        // 4. Read JSON response body
+        if (http.getResponseCode() == 200){
+            try (InputStream responseBody = http.getInputStream()){
+                InputStreamReader inputStreamReader = new InputStreamReader(responseBody);
+                // 5. Return the Result object
+                return new Gson().fromJson(inputStreamReader, ListGamesResult.class);
             }
         }
         else{
