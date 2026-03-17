1858722f08884fce638682c8dd7c9f414d80dd7f
blakeg9944 <blakeg9944@gmail.com>
2026-03-16 18:10:03 -0600

    Phase 5:  Starting Read Eval Print

--- client/src/main/java/client/ChessClient.java
+++ client/src/main/java/client/ChessClient.java
@@ -1,9 +1,31 @@
 package client;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 
 public class ChessClient {
+
+    private String authToken = null;
+    private final ServerFacade facade;
+
+    public ChessClient(String serverURL) {
+        this.facade = new ServerFacade(serverURL);
+    }
+
+    public void run(){
+        System.out.println("Welcome to Chess! Type 'Help' to begin.");
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine();
+            if (authToken == null) {
+                preLoginEval(input);
+            } else {
+                postLoginEval(input);
+            }
+        }
+    }
+
     public String help() {
         return """
             register <USERNAME> <PASSWORD> <EMAIL> - to create an account
@@ -11,5 +33,13 @@ public class ChessClient {
             quit - playing chess
             help - with possible commands
             """;
+    }
+
+    private void preLoginEval(String response){
+
+    }
+
+    private void postLoginEval(String input){
+
     }
 }
