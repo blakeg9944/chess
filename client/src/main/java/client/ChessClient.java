7de7749c6c2b4422e183176336df9f523a811d08
blakeg9944 <blakeg9944@gmail.com>
2026-03-18 11:45:58 -0600

    Phase 5:  Starting 2 logout

--- client/src/main/java/client/ChessClient.java
+++ client/src/main/java/client/ChessClient.java
@@ -1,7 +1,6 @@
 package client;
 
-import model.LoginRequest;
-import model.LoginResult;
+import model.*;
 
 import java.util.Arrays;
 import java.util.Scanner;
@@ -19,7 +18,7 @@ public class ChessClient {
     public void run() throws Exception {
         System.out.println("Welcome to Chess! Type 'Help' to begin.");
         Scanner scanner = new Scanner(System.in);
-        String result = '';
+        String result = "";
         while (!result.equals("quit")) {
             String input = scanner.nextLine();
             var tokens = input.toLowerCase().split(" ");
@@ -31,9 +30,9 @@ public class ChessClient {
             }
             var params = Arrays.copyOfRange(tokens, 1, tokens.length);
             if (authToken == null) {
-                preLoginEval(cmd, params);
+                result = preLoginEval(cmd, params);
             } else {
-                postLoginEval(input);
+                result = postLoginEval(cmd, params);
             }
         }
     }
@@ -60,8 +59,18 @@ public class ChessClient {
         }
     }
 
-    private void postLoginEval(String input){
-
+    private String postLoginEval(String command, String[] params){
+        try {
+            return switch (command) {
+                case "logout" -> logout();
+                case "list games" -> login(params);
+                case "join game" -> register(params);
+                case "create game" -> "quit";
+                default -> help();
+            };
+        } catch (Exception e) {
+            return e.getMessage();
+        }
     }
 
     private String login(String[] params) throws Exception {
@@ -76,7 +85,23 @@ public class ChessClient {
         throw new Exception("Expected: <USERNAME> <PASSWORD>");
     }
 
-    private void register(String username, String password, String email){
+    private String register(String[] params) throws Exception {
+        if(params.length == 3){
+            String username = params[0];
+            String password = params[1];
+            String email = params[2];
+            RegisterRequest registerRequest = new RegisterRequest(username, password, email);
+            RegisterResult registerResult = facade.register(registerRequest);
+            this.authToken = registerResult.authToken();
+            return String.format("Now registered as %s", username);
+        }
+        throw new Exception("Expected: <USERNAME> <PASSWORD> <EMAIL>");
+    }
 
+    private String logout() throws Exception {
+        LogoutRequest logoutRequest = new LogoutRequest(authToken);
+        facade.logout(logoutRequest);
+        this.authToken = null;
+        return "Now logged out";
     }
 }
