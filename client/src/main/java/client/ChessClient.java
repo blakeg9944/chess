89a4d19f61ceb6e1a44956af7f864dd734d91307
blakeg9944 <blakeg9944@gmail.com>
2026-03-18 00:41:58 -0600

    Phase 5:  Starting 2 Read Eval Print

--- client/src/main/java/client/ChessClient.java
+++ client/src/main/java/client/ChessClient.java
@@ -16,10 +16,19 @@ public class ChessClient {
     public void run(){
         System.out.println("Welcome to Chess! Type 'Help' to begin.");
         Scanner scanner = new Scanner(System.in);
-        while (true) {
+        String result = '';
+        while (!result.equals("quit")) {
             String input = scanner.nextLine();
+            var tokens = input.toLowerCase().split(" ");
+            String cmd;
+            if (tokens.length > 0) {
+                cmd = tokens[0];
+            } else {
+                cmd = "help";
+            }
+            var params = Arrays.copyOfRange(tokens, 1, tokens.length);
             if (authToken == null) {
-                preLoginEval(input);
+                preLoginEval(cmd, params);
             } else {
                 postLoginEval(input);
             }
@@ -35,11 +44,28 @@ public class ChessClient {
             """;
     }
 
-    private void preLoginEval(String response){
-
+    private String preLoginEval(String cmd, String[] params) throws Exception{
+        try {
+            return switch (cmd) {
+                case "login" -> login(params);
+                case "register" -> register(params);
+                case "quit" -> "quit";
+                default -> help();
+            };
+        } catch (Exception e) {
+            return e.getMessage();
+        }
     }
 
     private void postLoginEval(String input){
+
+    }
+
+    private void login(String username, String password){
+
+    }
+
+    private void register(String username, String password, String email){
 
     }
 }
