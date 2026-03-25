0c520af87c5bf19f60d3946fada49eedf33515f9
blakeg9944 <blakeg9944@gmail.com>
2026-03-25 15:13:37 -0600

    Phase 6: Finsihed Message classes, working on connection manager

--- notes.md
+++ notes.md
@@ -1,1 +1,6 @@
 # My notes
+The ConnectionManager: You already started thinking about this! This is the class that stores who is in which game.
+
+The WebSocketHandler: This is the "Front Door." It receives the JSON text, turns it back into your UserGameCommand objects, and decides which method to call.
+
+The Game Logic Integration: This is where you connect your existing ChessGame logic (from Phase 3/4) to the WebSocket so the board actually updates.
--- server/src/main/java/server/websocket/ConnectionManager.java
+++ server/src/main/java/server/websocket/ConnectionManager.java
@@ -3,17 +3,36 @@ package server.websocket;
 import com.mysql.cj.Session;
 import websocket.messages.ServerMessage;
 
-import java.util.List;
+import java.util.Collections;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class ConnectionManager {
 
     // Instead of one big list, we group sessions by their Game ID
-    ConcurrentHashMap<Integer, List<Session>> gameRooms = new ConcurrentHashMap<>();
+    ConcurrentHashMap<Integer, Set<Session>> gameRooms = new ConcurrentHashMap<>();
 
     public void broadcast(int gameID, Session excludeSession, ServerMessage serverMessage){
         // 1. Find the specific list of players for this gameID
         // 2. Loop ONLY through those players
         // 3. Send the message (if they aren't the excludeSession)
+    }
+
+    public void addSessionToGame(int gameID, Session session){
+        Set<Session> sessions = gameRooms.computeIfAbsent(gameID, k -> Collections.newSetFromMap(new ConcurrentHashMap<>()));
+    }
+
+    public void removeSessionFromGame(int gameID, Session session){
+        Set<Session> sessionSet = getSessionsForGame(gameID);
+        if (sessionSet != null){
+            sessionSet.remove(session);
+            if (sessionSet.isEmpty()){
+                gameRooms.remove(gameID);
+            }
+        }
+    }
+
+    public Set<Session> getSessionsForGame(int gameID){
+        return gameRooms.get(gameID);
     }
 }
